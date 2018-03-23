8d97a2ea10b704fe0e35e39568cf86a844dee137
Lisa Trevis <l_trevis@hotmail.com>
2018-03-23 10:41:38 -0700

    finished sections

--- js/main.js
+++ js/main.js
@@ -1,83 +1,79 @@
-const firstView = {
-	template: `
-	    <div class="firstView">
-	    	<div class="row">
-				<div class="col">
-					<img src="img/firequeenthumb.jpg" alt="..." class="img-thumbnail img-fluid">
-				</div>
-				<div class="col">
-					<img src="img/candythumb.jpg" alt="..." class="img-thumbnail img-fluid">
-				</div>
-				<div class="col">
-					<img src="img/rainbowpeacedreadbead.jpg" alt="..." class="img-thumbnail img-fluid">
-				</div>
-			</div>
-	      	<router-view></router-view>
-	    </div>
-	`
+const sample = {
+	template: `<div class="text-center">
+					<div class="row">
+						<div class="col">
+							<img src="img/dragonflythumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
+						</div>
+						<div class="col">
+							<img src="img/candythumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
+						</div>
+						<div class="col">
+							<img src="img/rainbowpeacedreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
+						</div>
+					</div>
+				</div>`
 }
-
 const all = {
 	template: `
 		<div class="text-center">
 			<div class="row">
 				<div class="col">
-					<img src="img/dragonflythumb.jpg" alt="..." class="img-thumbnail">
+					<img src="img/dragonflythumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 				<div class="col">
-					<img src="img/firequeenthumb.jpg" alt="..." class="img-thumbnail">
+					<img src="img/firequeenthumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 				<div class="col">
-					<img src="img/flamequeenthumb.jpg" alt="..." class="img-thumbnail">
+					<img src="img/flamequeenthumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 				<div class="col">
-					<img src="img/honeybeethumb.jpg" alt="..." class="img-thumbnail">
+					<img src="img/honeybeethumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 			</div>
 			
 			<div class="row">
 				<div class="col">
-					<img src="img/candythumb.jpg" alt="..." class="img-thumbnail img-fluid">
+					<img src="img/candythumb.jpg" alt="..." class="img-thumbnail  img-fluid img-responsive">
 				</div>
 				<div class="col">
-					<img src="img/oceanthumb.jpg" alt="..." class="img-thumbnail img-fluid">
+					<img src="img/oceanthumb.jpg" alt="..." class="img-thumbnail  img-fluid img-responsive">
 				</div>
 				<div class="col">
-					<img src="img/southwestthumb.jpg" alt="..." class="img-thumbnail img-fluid">
+					<img src="img/southwestthumb.jpg" alt="..." class="img-thumbnail  img-fluid img-responsive">
 				</div>
 			</div>
 
 			<div class="row">
 				<div class="col">
-					<img src="img/bluecopperdreadbead.jpg" alt="..." class="img-thumbnail">
+					<img src="img/bluecopperdreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 				<div class="col">
-					<img src="img/clearsilverdreadbead.jpg" alt="..." class="img-thumbnail">
+					<img src="img/clearsilverdreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 				<div class="col">
-					<img src="img/goldpeacedreadbead.jpg" alt="..." class="img-thumbnail">
-				</div>
-			</div>
-			<div class="row">
-				<div class="col">
-					<img src="img/goldpurpledreadbead.jpg" alt="..." class="img-thumbnail">
-				</div>
-				<div class="col">
-					<img src="img/greensilverswirldreadbead.jpg" alt="..." class="img-thumbnail">
-				</div>
-				<div class="col">
-					<img src="img/icebluepeacedreadbead.jpg" alt="..." class="img-thumbnail">
+					<img src="img/goldpeacedreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 			</div>
 			<div class="row">
 				<div class="col">
-					<img src="img/bluesilverdreadbead.jpg" alt="..." class="img-thumbnail">
+					<img src="img/goldpurpledreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 				<div class="col">
-					<img src="img/greenlaughdreadbead.jpg" alt="..." class="img-thumbnail">
+					<img src="img/greensilverswirldreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 				<div class="col">
-					<img src="img/rainbowpeacedreadbead.jpg" alt="..." class="img-thumbnail">
+					<img src="img/icebluepeacedreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col">
+					<img src="img/bluesilverdreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
+				</div>
+				<div class="col">
+					<img src="img/greenlaughdreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
+				</div>
+				<div class="col">
+					<img src="img/rainbowpeacedreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 				</div>
 			</div>
 		</div>
@@ -87,16 +83,16 @@ const wings = {
 	template: `<div class="text-center">
 					<div class="row">
 						<div class="col">
-							<img src="img/dragonflythumb.jpg" alt="..." class="img-thumbnail">
+							<img src="img/dragonflythumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 						<div class="col">
-							<img src="img/firequeenthumb.jpg" alt="..." class="img-thumbnail">
+							<img src="img/firequeenthumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 						<div class="col">
-							<img src="img/flamequeenthumb.jpg" alt="..." class="img-thumbnail">
+							<img src="img/flamequeenthumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 						<div class="col">
-							<img src="img/honeybeethumb.jpg" alt="..." class="img-thumbnail">
+							<img src="img/honeybeethumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 					</div>
 				</div>`
@@ -105,13 +101,13 @@ const coats = {
 	template: `<div class="text-center">
 					<div class="row">
 						<div class="col">
-							<img src="img/candythumb.jpg" alt="..." class="img-thumbnail img-fluid">
+							<img src="img/candythumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 						<div class="col">
-							<img src="img/oceanthumb.jpg" alt="..." class="img-thumbnail img-fluid">
+							<img src="img/oceanthumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 						<div class="col">
-							<img src="img/southwestthumb.jpg" alt="..." class="img-thumbnail img-fluid">
+							<img src="img/southwestthumb.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 					</div>
 				</div>`
@@ -120,62 +116,61 @@ const jewelry = {
 	template: `<div class="text-center">
 					<div class="row">
 						<div class="col">
-							<img src="img/bluecopperdreadbead.jpg" alt="..." class="img-thumbnail">
+							<img src="img/bluecopperdreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 						<div class="col">
-							<img src="img/clearsilverdreadbead.jpg" alt="..." class="img-thumbnail">
+							<img src="img/clearsilverdreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 						<div class="col">
-							<img src="img/goldpeacedreadbead.jpg" alt="..." class="img-thumbnail">
-						</div>
-					</div>
-					<div class="row">
-						<div class="col">
-							<img src="img/goldpurpledreadbead.jpg" alt="..." class="img-thumbnail">
-						</div>
-						<div class="col">
-							<img src="img/greensilverswirldreadbead.jpg" alt="..." class="img-thumbnail">
-						</div>
-						<div class="col">
-							<img src="img/icebluepeacedreadbead.jpg" alt="..." class="img-thumbnail">
+							<img src="img/goldpeacedreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 					</div>
 					<div class="row">
 						<div class="col">
-							<img src="img/bluesilverdreadbead.jpg" alt="..." class="img-thumbnail">
+							<img src="img/goldpurpledreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 						<div class="col">
-							<img src="img/greenlaughdreadbead.jpg" alt="..." class="img-thumbnail">
+							<img src="img/greensilverswirldreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 						<div class="col">
-							<img src="img/rainbowpeacedreadbead.jpg" alt="..." class="img-thumbnail">
+							<img src="img/icebluepeacedreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
+						</div>
+					</div>
+					<div class="row">
+						<div class="col">
+							<img src="img/bluesilverdreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
+						</div>
+						<div class="col">
+							<img src="img/greenlaughdreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
+						</div>
+						<div class="col">
+							<img src="img/rainbowpeacedreadbead.jpg" alt="..." class="img-thumbnail img-fluid img-responsive">
 						</div>
 					</div>
 				</div>`
 }
 
 const router = new VueRouter({
-	routes: [
+	routes: [ 		
 		{
-			path: '/firstView/:id', component: firstView,
-	      		children: [
-					{
-						path: '/all',
-						component: all
-					},
-					{
-						path: '/wings',
-						component: wings
-					},
-					{
-						path: '/coats',
-						component: coats
-					},
-					{
-						path: '/jewelry',
-						component: jewelry
-					}
-			]
+			path: '/sample',
+			component: sample
+		},
+		{
+			path: '/all',
+			component: all
+		},
+		{
+			path: '/wings',
+			component: wings
+		},
+		{
+			path: '/coats',
+			component: coats
+		},
+		{
+			path: '/jewelry',
+			component: jewelry
 		}
 	]
 })
@@ -188,3 +183,5 @@ var routeTest = new Vue({
 	methods: {
 	}
 }).$mount('#app')
+
+router.replace({ path: '/sample', redirect: '/' })
